ad4a7c77a2f2e4b15ec33b30c4aa4664a4bce81e
Lucas Garofolo <lucas.garofolo@gmail.com>
2025-01-17 14:44:40 -0300

    opcao3

--- src/server.js
+++ src/server.js
@@ -25,56 +25,47 @@ function start(client) {
   client.onMessage((message) => {
     if (!message.isGroupMsg) {
 
-      //console.log(message);
-
-      /*teste envio single
-      client.sendText(message.from, "oi").then(() => {
-        console.log('Message sent.');
-      }).catch(error => console.error('Error when sending message', error));
-      */
-
       switch (message.body) {
-        case '1':
-          client.sendVoice(message.from, './teste.mp3').then((result) => {
+        case '1':  //Informações Gerais
+          var info = "🌟 *Informações Gerais da Escola* 🌟 \n\n 📅 *Horário de Funcionamento:* \n Segunda a Sexta: 08h00 às 20h00 \n Sábados: 08h00 às 12h00 \n\n 📍 *Endereço:* \n Rua São Tomás, 01 - Jardim São José - Cruzeiro/SP \n\n 📞 *Telefone:* \n (12) 3141-1400 \n\n 🟢 *WhatsApp:* \n (12) 99653-2560 \n\n 🌐 *Redes Sociais e Site:* \n 🔗 Instagram: @senaicruzeiro \n 🔗 Facebook: fb.com/escolasenaicruzeiro \n 🔗 Site Oficial: https://sp.senai.br/unidade/cruzeiro/ \n\n Estamos à disposição para mais informações! 😊";
+          client.sendText(message.from, info).then((result) => {
             console.log('Result: ', result); //return object success
           }).catch((erro) => {
             console.error('Error when sending: ', erro); //return object error
           });
-          break;
+          client.sendLocation(message.from, '-22.58786', '-44.97554', 'SENAI - Cruzeiro').then((result) => {
+            console.log('Result: ', result); //return object success
+          })
+          .catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
+          break; 
+        
 
         case '2':
-          // Send Messages with Buttons Reply
-          const buttons = [
-            {
-              "buttonText": {
-                "displayText": "Text of Button 1"
-              }
-            },
-            {
-              "buttonText": {
-                "displayText": "Text of Button 2"
-              }
-            }
-          ]
-          client.sendButtons(message.from, 'Title', buttons, 'Description')
+          client
+            .sendFile(
+              message.from,
+              './pdftest.pdf',
+              'file_name',
+              'Vou te mandar um pdf, dá uma olhada!'
+            )
             .then((result) => {
               console.log('Result: ', result); //return object success
             })
             .catch((erro) => {
               console.error('Error when sending: ', erro); //return object error
             });
-
           break;
 
         case '3':
-          //-10.7922474, -37.7556548
-          client.sendLocation(message.from, '-10.7922474', '-37.7556548', 'Brasil')
-            .then((result) => {
-              console.log('Result: ', result); //return object success
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro); //return object error
-            });
+          // Send Messages with Buttons Reply
+          var info = "📢 Atenção, futuros alunos! 📝✨ \n \n Para realizar a sua matrícula conosco, é super simples! Você só precisa trazer: \n\n 📄 RG e CPF ou CNH \n 🏠 Comprovante de residência \n 🎓 Comprovante de escolaridade \n\n ⚠️ Importante! Alguns cursos possuem pré-requisitos específicos, podendo exigir outros documentos adicionais. 🧐📚 \n \n 📬 Ficou com dúvidas? Estamos à disposição para ajudar! 💬😊";
+          client.sendText(message.from, info).then((result) => {
+            console.log('Result: ', result); //return object success
+          }).catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
           break;
 
         case '4':
@@ -109,54 +100,8 @@ function start(client) {
             });
           break;
 
-        case '6':
-          client
-            .sendFile(
-              message.from,
-              './pdftest.pdf',
-              'file_name',
-              'Vou te mandar um pdf, dá uma olhada!'
-            )
-            .then((result) => {
-              console.log('Result: ', result); //return object success
-            })
-            .catch((erro) => {
-              console.error('Error when sending: ', erro); //return object error
-            });
-          break;
-
-        case '7':
-          client.setProfileStatus('Josh - Tô on! Manda ver! ✈️');
-          client.sendText(message.from, "Status do perfil alterado!").then(() => {
-            console.log('Message sent.');
-          }).catch(error => console.error('Error when sending message', error));
-          break;
-
-        case '8':
-          client.setProfileStatus('Josh - Estamos fechados!!! abriremos amanhã as 9h horas!');
-          client.sendText(message.from, "Status do perfil alterado!").then(() => {
-            console.log('Message sent.');
-          }).catch(error => console.error('Error when sending message', error));
-          break;
-
-        case '9':
-          client.setProfilePic('./store-open.jpg');
-          client.sendText(message.from, "Foto de perfil aberto").then(() => {
-            console.log('Message sent.');
-          }).catch(error => console.error('Error when sending message', error));
-          break;
-
-        case '10':
-          client.setProfilePic('./store-close.jpg');
-          client.sendText(message.from, "Foto do perfil fechado").then(() => {
-            console.log('Message sent.');
-          }).catch(error => console.error('Error when sending message', error));
-          break;
-
-
-
         default:
-          var menu = '👋 Olá, como vai? \n\nEu sou GPT390, o *assistente virtual* do SENAI - Cruzeiro. \n* Como posso te ajudar?* 🙋‍♂️ ';
+          var menu = '👋 Olá, como vai? \n\nEu sou GPT390, o *assistente virtual* do SENAI - Cruzeiro. \n* Como posso te ajudar?* 🙋‍♂️\n *Escolha uma opção de 1 a 5* \n-----------------------------------\n1️⃣ - ``Informações Gerais`` \n2️⃣ - ``Cursos Oferecidos``\n3️⃣ - ``Processo de Inscrição``\n4️⃣ - ``Contato``\n5️⃣ - ``Recado do Diretor``\n';
           client.sendText(message.from, menu).then(() => {
             console.log('Message sent.');
           }).catch(error => console.error('Error when sending message', error));
@@ -165,4 +110,5 @@ function start(client) {
 
     }
   });
+  //0️⃣
 };
